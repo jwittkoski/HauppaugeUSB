24b311732fc87967785b1719b1c7291de3f3266e
John Wittkoski <jwittkoski@gmail.com>
2020-09-27 14:42:06 -0400

    fix patches

---
+++ Patches/apply_patches.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for fl in 01-NewLine.patch \
+    02-string.patch \
+    03-EnableRegisteredParameters.patch \
+    04-SplitLoggingLevels.patch \
+    05-FirmwareLocation.patch \
+    06-AVOutputCallback.patch \
+    07-ThreadName.patch \
+    08-AudioCS8416.patch
+do
+    patch -p1 "../HauppaugeUSB/Patches/${fl}"
+done
